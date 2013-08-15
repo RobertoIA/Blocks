663ce4e88631fb31b4a3e26e945c7c36ab2b9d0b
Roberto Izquierdo Amo <RobertoIA1987@gmail.com>
2013-08-15 19:23:51 +0200

    Added basic program structure.

--- blocks.js
+++ blocks.js
@@ -1,10 +1,35 @@
+/*global console, window, document*/
+
+// Constants
+var FPS = 30;
+
 var canvas = null;
 var context = null;
 
-// Kicks in once the DOM has been loaded.
-window.onload = function () {
+var setup = function () {
+    'use strict';
     canvas = document.getElementById("canvas");
     context = canvas.getContext('2d');
-    canvas.width = 800;
-    canvas.height = 600;
+    canvas.width = 200;
+    canvas.height = 200;
+    
+    console.log('Setup completed.');
+};
+
+var mainloop = function () {
+    'use strict';
+    console.log('Starting mainloop');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    
+    context.rect(0, 0, canvas.width, canvas.height);
+    context.stroke();
+    
+    console.log('Mainloop finished');
+};
+
+// Kicks in once the DOM has been loaded.
+window.onload = function () {
+    'use strict';
+    setup();
+    window.setInterval(mainloop, 1000 / FPS);
 };
