d8273e8938ad1f325f7af06b6f273df75e423847
Roberto Izquierdo Amo <RobertoIA1987@gmail.com>
2013-08-17 20:46:16 +0200

    Loads sprite JSON data.
    Loading asychronous for now.

--- blocks.js
+++ blocks.js
@@ -6,7 +6,30 @@ var FPS = 30;
 var canvas = null;
 var context = null;
 
-var L = new Image();
+var sprites = [];
+
+var load = function () {
+    'use strict';
+    var xhr = new XMLHttpRequest();
+    var spriteData;
+    xhr.open('GET', 'sprites.json', false); // not asynchronous
+    xhr.onload = function () {
+        spriteData = JSON.parse(this.responseText);
+    }
+    xhr.send();
+    
+    /*
+    sprites['I'] = ;
+    sprites['O'] = ;
+    sprites['J'] = ;
+    sprites['L'] = ;
+    sprites['S'] = ;
+    sprites['Z'] = ;
+    sprites['T'] = ;
+    */
+    
+    console.log('Loading completed.');
+};
 
 var setup = function () {
     'use strict';
@@ -15,27 +38,23 @@ var setup = function () {
     canvas.width = 200;
     canvas.height = 200;
     
-    L.src = "L.png";
-    
     console.log('Setup completed.');
 };
 
 var mainloop = function () {
     'use strict';
-    console.log('Starting mainloop');
     context.clearRect(0, 0, canvas.width, canvas.height);
     
     context.rect(0, 0, canvas.width, canvas.height);
     context.stroke();
     
-    context.drawImage(L, 0, 0);
-    
-    console.log('Mainloop finished');
+    // context.drawImage(sprites['L'], 0, 0);
 };
 
 // Kicks in once the DOM has been loaded.
 window.onload = function () {
     'use strict';
+    load();
     setup();
     window.setInterval(mainloop, 1000 / FPS);
 };
