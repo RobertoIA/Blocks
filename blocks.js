a9e1c22f318c4d1d28de41aae673ee218c3f33ea
Roberto Izquierdo Amo <RobertoIA1987@gmail.com>
2013-08-18 19:44:14 +0200

    Fixed sprite numbering.

--- blocks.js
+++ blocks.js
@@ -13,7 +13,6 @@ var load = function () {
     'use strict';
     var xhr = new XMLHttpRequest(),
         spriteData,
-        spriteName,
         sprite;
     
     // Loads spritesheet.
@@ -25,8 +24,13 @@ var load = function () {
         spriteData = JSON.parse(this.responseText);
     };
     xhr.send();
-    sprites = spriteData.frames;
     
+    for (sprite in spriteData.frames) {
+        if (spriteData.frames.hasOwnProperty(sprite)) {
+            sprites.push(spriteData.frames[sprite]);
+        }
+    }
+
     console.log('Loading completed.');
 };
 
@@ -42,42 +46,18 @@ var setup = function () {
 
 var mainloop = function () {
     'use strict';
-    var nextBlock = Math.floor(Math.random() * 7),
-        nextBlockSprite;
-    
-    switch(nextBlock) {
-            case 0:
-                nextBlockSprite = sprites['I.png'].frame;
-                break;
-            case 1:
-                nextBlockSprite = sprites['O.png'].frame;
-                break;
-            case 2:
-                nextBlockSprite = sprites['J.png'].frame;
-                break;
-            case 3:
-                nextBlockSprite = sprites['L.png'].frame;
-                break;
-            case 4:
-                nextBlockSprite = sprites['S.png'].frame;
-                break;
-            case 5:
-                nextBlockSprite = sprites['Z.png'].frame;
-                break;
-            case 6:
-                nextBlockSprite = sprites['T.png'].frame;
-                break;
-    }
+    var nextBlock = sprites[Math.floor(Math.random() * 7)].frame;
     
     context.clearRect(0, 0, canvas.width, canvas.height);
     
     context.rect(0, 0, canvas.width, canvas.height);
     context.stroke();
 
-    context.drawImage(spriteSheet, nextBlockSprite.x, nextBlockSprite.y,
-                      nextBlockSprite.w, nextBlockSprite.h,
+    context.drawImage(spriteSheet,
+                      nextBlock.x, nextBlock.y,
+                      nextBlock.w, nextBlock.h,
                       0, 0,
-                      nextBlockSprite.w, nextBlockSprite.h);
+                      nextBlock.w, nextBlock.h);
 };
 
 // Kicks in once the DOM has been loaded.
